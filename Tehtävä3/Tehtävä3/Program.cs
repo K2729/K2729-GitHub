8cf70cb6573e70ea72703f582394595278ed6589
K2729 <K2729@student.jamk.fi>
2016-01-13 13:09:35 +0200

    Testing

---
+++ Tehtävä3/Tehtävä3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub");
+        }
+    }
+}
