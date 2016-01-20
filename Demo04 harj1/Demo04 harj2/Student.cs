c1492e891aafa35e856d3f941d22b090bdc4f530
K2729 <K2729@student.jamk.fi>
2016-01-20 14:19:21 +0200

    ok

---
+++ Demo04 harj1/Demo04 harj2/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04_harj2
+{
+    class Student : Person
+    {
+        public string Id { get; set; }
+
+        public Student()
+        {
+
+        }
+        public Student(string firstName, string lastName, string id) 
+            :base(firstName, lastName)
+        {
+            Id = id;
+        }
+        public void StudentMethod()
+        {
+            Console.WriteLine("Teacher does something...");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " " + Id;
+        }
+    }
+}
---
+++ Demo04 harj1/Demo04 harj2/Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04_harj2
+{
+    class Teacher : Person
+    {
+        public string Room { get; set; }
+
+        public Teacher()
+        {
+
+        }
+        public Teacher(string firstName, string lastName, string room) 
+            : base(firstName,lastName)
+        {
+            Room = room;
+        }
+        public void TeacherMethod()
+        {
+            Console.WriteLine("Teacher does something...");
+        }
+        
+        public override string ToString()
+        {
+            return base.ToString() + " " + Room;
+        }
+
+    }
+}
