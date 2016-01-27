3df2ff6dd22cec2ca69f66c4c3c3a90660ae8f5c
K2729 <K2729@student.jamk.fi>
2016-01-27 11:52:25 +0200

    45

---
+++ Demo05/demo05 teht4/Bike.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo05_teht4
+{
+    class Bike
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+
+        public List<Specs> Specs { get; }
+
+        public Bike()
+        {
+            Specs = new List<Specs>();
+        }
+        public void AddSpec(Specs spec)
+        {
+            Specs.Add(spec);
+        }
+        public override string ToString()
+        {
+            string sBike = "Bike : " + Name + "\n\nModel : " + Model + "\n\nSpecs :\n\n";
+            foreach (Specs spec in Specs)
+            {
+                sBike += spec.ToString() + "\n";
+            }
+            return sBike;
+        }
+
+     
+
+
+    }
+}
---
+++ Demo05/demo05 teht4/Specs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo05_teht4
+{
+    class Specs
+    {
+        public string Speed { get; set; }
+        public string Acce { get; set; }
+        public string Engine { get; set; }
+
+        public override string ToString()
+        {
+            return "Top speed :" + Speed + "Acceleration :" + Acce + "Engine :" + Engine;
+        }
+
+    }
+}
