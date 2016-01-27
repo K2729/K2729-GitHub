a2f28a51dd3798fa244298e92d42a62300e82c93
K2729 <K2729@student.jamk.fi>
2016-01-27 15:35:00 +0200

    ii

---
+++ Demo06/demo06 harj/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo06_harj
+{
+    /// <summary>
+    /// This class contains Person properties.
+    /// </summary>
+    class Person
+    {
+        public string Firtsname { get; set; }
+        public string Lastname { get; set; }
+        public string SocialSecurityNumber { get; set; }
+
+        public override string ToString()
+        {
+            return Firtsname + " " + Lastname + " " + SocialSecurityNumber;
+        }
+
+    }
+}
---
+++ Demo06/demo06 teht2/Cd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo06_teht2
+{
+    class Cd
+    {
+        private List<Song> songs;
+        public double Prize { get; set; }
+        public void Songs()
+        {
+            songs = new List<Song>();
+        }
+
+        public void AddSong(Song song)
+        {
+            songs.Add(song);
+        }
+        public void PrintCollection()
+        {
+            Console.WriteLine("Song and time: ");
+            foreach (Song song in songs)
+            {
+                Console.WriteLine(song.ToString());
+            }
+        }
+
+    }
+}
