4502ef29da1cb1caf117dd1c83f869cbe2384b71
K2729 <K2729@student.jamk.fi>
2016-02-10 15:04:49 +0200

    GUI

---
+++ Demo10/Teht 0/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Teht_0
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+
+
+              
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Name is " + nameTextBox.Text);
+            Debug.WriteLine("Email is " + emailBox.Text);
+
+            if ((bool) normalUserCheckBox.IsChecked)
+            {
+                Debug.WriteLine("Normal user!");
+            }
+        }
+    }
+}
